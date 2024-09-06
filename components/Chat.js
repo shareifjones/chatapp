3e863736a3371a8803db3904f06f6ac4ee367c72
shareifjones <shareifjones@gmail.com>
2024-09-06 15:40:04 -0400

    5.2

--- components/Chat.js
+++ components/Chat.js
@@ -72,7 +72,7 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        // justifyContent: 'center',
         // alignItems: 'center'
     }
 });
